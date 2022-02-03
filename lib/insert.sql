908b8d657108028359c286724427e077db2ac284
Daniel <dkilcullen25@gmail.com>
2022-02-03 11:50:28 -0600

    commit tree 331887d4ec723b24ad2bd40857b1d827f5758f4b

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+insert into bears (id,name,age,sex,color,temperament,alive)
+values (1,'Mr. Chocolate',10,'M','green','nice',true);
+insert into bears (id,name,age,sex,color,temperament,alive)
+values (2,'Rowdy',10,'M','blue','nice',true);
+insert into bears (id,name,age,sex,color,temperament,alive)
+values (3,'Tabitha',10,'M','red','nice',true);
+insert into bears (id,name,age,sex,color,temperament,alive)
+values (4,'Sergeant Brown',10,'M','orange','nice',true);
+insert into bears (id,name,age,sex,color,temperament,alive)
+values (5,'Melissa',10,'M','teal','nice',true);
+insert into bears (id,name,age,sex,color,temperament,alive)
+values (6,'Grinch',10,'M','purple','mean',false);
+insert into bears (id,name,age,sex,color,temperament,alive)
+values (7,'Wendy',10,'M','red','nice',true);
+insert into bears (id,age,sex,color,temperament,alive)
+values (8,10,'M','orange','nice',true); 
